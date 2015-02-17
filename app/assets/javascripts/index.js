7e06ab44042e0d2904e23f9ade9fec01b3c11ddc
son <son@lincolnix.net>
2015-02-17 01:04:51 +0000

    style updates

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $(".entry-content").hide();
     
     $(".title").click(function () {
-	console.log($(this).parent().siblings("div.entry-content"));
+	//console.log($(this).parent().siblings("div.entry-content"));
 	$(this).parent().siblings("div.entry-content").toggle();
     });
       
