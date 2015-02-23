fff03481b6b7f702884577f954d2521a3293980b
son <son@lincolnix.net>
2015-02-23 15:55:19 +0000

    assets

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,12 @@
 
-//$(document).ready ( function () {
+$(document).ready ( function () {
 
     $("div.entry-content").hide();
     
     $('a[href="#"]').click( function(e) {
 	e.preventDefault();
     });
-
+});
 
     $(".title").click(function (e) {
 	var entry = $(this).parent().siblings("div.entry-content");
